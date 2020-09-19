790d2d9722bb8b62a3fe4ed3fa9c28a917bb3903
UNREALre <avpmanager@gmail.com>
2020-09-19 18:13:23 +0400

    Created UserSource model.

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from article.models import Source
+
+
+class UserSource(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    login = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
