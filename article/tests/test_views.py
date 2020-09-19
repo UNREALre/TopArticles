f428bc103d4a2fe18aa0f26b50f844830b88cf91
UNREALre <avpmanager@gmail.com>
2020-09-19 17:00:07 +0400

    Implemented Unit tests for source list and source get single.

--- article/tests/test_views.py
+++ article/tests/test_views.py
@@ -1,10 +1,51 @@
-import json
 from rest_framework import status
+from rest_framework.test import APIClient
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
 from ..models import Source
 from ..serializers import SourceSerializer
 
 
-# Initialize the API Client
-client = Client()
+class GetAllSourcesTest(TestCase):
+    """ Test module for GET all sources API """
+
+    def setUp(self):
+        self.client = APIClient()
+        Source.objects.create(name='Source1', feed_url='https://site1.com')
+        Source.objects.create(name='Source2', feed_url='https://site2.com')
+        Source.objects.create(name='Source3', feed_url='https://site3.com')
+
+    def test_get_all_sources(self):
+        response = self.client.get(reverse('article:sources_list'))
+        sources = Source.objects.all()
+        serializer = SourceSerializer(sources, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetSingleSourceTest(TestCase):
+    """ Test module for GET single source API """
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
+        self.token = Token.objects.create(user=self.user)
+
+        self.src1 = Source.objects.create(name='Source1', feed_url='https://site1.com')
+        self.src2 = Source.objects.create(name='Source2', feed_url='https://site2.com')
+        self.src3 = Source.objects.create(name='Source3', feed_url='https://site3.com')
+
+    def test_get_valid_single_source(self):
+        # self.client.force_login(user=self.user)
+        # response = self.client.get(
+        #    reverse('article:source_view_update_delete', kwargs={'pk': self.src1.pk}),
+        #    HTTP_AUTHORIZATION=self.token)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.get(reverse('article:source_view_update_delete', kwargs={'pk': self.src1.pk}))
+        source = Source.objects.get(pk=self.src1.pk)
+        serializer = SourceSerializer(source)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- article/urls.py
+++ article/urls.py
@@ -8,5 +8,5 @@ app_name = 'article'
 urlpatterns = [
     path('sources/', SourceView.as_view(), name='sources_list'),
     path('sources/create/', SourceCreateView.as_view(), name='source_create'),
-    path('sources/<int:pk>/', SingleSourceView.as_view(), name='source_update_delete')
+    path('sources/<int:pk>/', SingleSourceView.as_view(), name='source_view_update_delete')
 ]
