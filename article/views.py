7dfb90660b793ff18b97b33074064ac0483dc157
UNREALre <avpmanager@gmail.com>
2020-09-20 10:30:49 +0400

    Restricted Article list/detail APIs to contain only user related articles.

--- article/views.py
+++ article/views.py
@@ -9,17 +9,23 @@ from .serializers import (SourceSerializer, ArticleSerializer,
 
 
 class SourceView(ListAPIView):
+    """View for source action list API"""
+
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
 
 
 class SourceCreateView(CreateAPIView):
+    """View for source action create API"""
+
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
     permission_classes = (IsAdminUser, )
 
 
 class SingleSourceView(RetrieveUpdateDestroyAPIView):
+    """View for source actions: detail/update/delete API"""
+
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
     permission_classes = (IsAdminUser, )
@@ -52,22 +58,35 @@ class ArticlePermission(permissions.BasePermission):
 class ArticleViewSet(viewsets.ModelViewSet):
     """Main View Set for all CRUD operations with Articles"""
 
-    queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     permission_classes = (ArticlePermission, )
 
+    def get_queryset(self):
+        """Restricting queryset to contain only user related articles"""
+
+        user = self.request.user
+        return user.articles.all()
+
     def list(self, request, *args, **kwargs):
+        """Specifying special serializer for the list action"""
+
         self.serializer_class = ArticleListSerializer
         return super(ArticleViewSet, self).list(request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
+        """Specifying special serializer for the detail action"""
+
         self.serializer_class = ArticleDetailSerializer
         return super(ArticleViewSet, self).retrieve(request, *args, **kwargs)
 
     def perform_create(self, serializer):
+        """Before create - retrieving source from DB based on source_id passed through API"""
+
         source = get_object_or_404(Source, id=self.request.data.get('source_id'))
         return serializer.save(source=source)
 
     def perform_update(self, serializer):
+        """Before update - retrieving source from DB based on source_id passed through API"""
+
         source = get_object_or_404(Source, id=self.request.data.get('source_id'))
         return serializer.save(source=source)
