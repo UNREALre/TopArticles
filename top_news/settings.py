372db47b2e4109f73361f327e83af4cf0acbe7f9
UNREALre <avpmanager@gmail.com>
2020-09-20 10:57:16 +0400

    Base logging format fix.

--- top_news/settings.py
+++ top_news/settings.py
@@ -122,11 +122,22 @@ AUTH_PASSWORD_VALIDATORS = [
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
     'handlers': {
         'file': {
             'level': 'INFO',
             'class': 'logging.FileHandler',
             'filename': os.path.join(BASE_DIR, 'logs/main.log'),
+            'formatter': 'verbose'
         },
     },
     'loggers': {
