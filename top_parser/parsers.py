b29b85cb403d3d2ff7acae2b8fbea7b163c2837f
UNREALre <avpmanager@gmail.com>
2020-09-21 23:48:35 +0400

    VC parser completed.

--- top_parser/parsers.py
+++ top_parser/parsers.py
@@ -76,6 +76,7 @@ class HabrParser:
         #    articles = executor.map(self.parse_page, pages_with_arts)
 
         articles = list()
+        pages_with_arts = pages_with_arts[:3]  # for fast debug
         for page in pages_with_arts:
             articles.append(self.parse_page(page))
 
@@ -147,7 +148,7 @@ class HabrParserBuilder:
 
     def authorize(self, login, password):
         """
-        Method authorize user within HabraHabr and return path to file with cookies.
+        Method authorize user within HabraHabr and return selenium driver object with all required cookies.
 
         Receives user login and password. Generates random fake user agent.
         Using selenium authorizes user, retrieves cookies.
@@ -174,11 +175,99 @@ class HabrParserBuilder:
 
 
 class VcParser:
-    def __init__(self, access_token):
-        self.access_token = access_token
+    """Parse for VC.RU resource."""
+
+    def __init__(self, driver):
+        self.driver = driver
 
     def test_connection(self):
-        pass
+        """Test if authentication to source with user credentials went fine."""
+
+        auth_flag = False
+        if self.driver.get_cookies():
+            cookies = self.driver.get_cookies()
+            for cookie in cookies:
+                if cookie.get('name') == 'osnova-aid':
+                    auth_flag = True
+                    break
+
+        return auth_flag
+
+    def do_parse(self, url):
+        articles = list()
+
+        sleep(1)
+
+        height = self.driver.execute_script("return document.body.scrollHeight")
+        # Simulation of scrolling down process, until all articles will be shown
+        while True:
+            body = self.driver.find_element_by_tag_name('body')
+            body.send_keys(Keys.END)
+
+            sleep(2)
+
+            current_height = self.driver.execute_script("return document.body.scrollHeight")
+            if current_height != height:
+                height = current_height
+            else:
+                # no more articles to load
+                break
+
+        page_html = self.driver.page_source
+        soup = BeautifulSoup(page_html, 'lxml')
+        articles = soup.find_all('div', {'class': 'feed__item l-island-round'})
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            page_articles = executor.map(self.parse_article, articles)
+
+        self.driver.close()
+
+        return page_articles
+
+    def parse_article(self, article):
+        parsed_article = dict()
+
+        author = article.find('div', {'class': 'content-header-author__name'})
+        if author and author.get_text().strip() != "Промо":
+            h2 = article.find('h2')
+            full_link = article.find('a', {'class': 'content-feed__link'})
+            href = full_link['href'] if full_link else ''
+
+            if href:
+                try:
+                    article = Article.objects.get(url=href)
+                except Article.DoesNotExist:
+                    article = None
+
+                if not article:
+                    try:
+                        detail = BeautifulSoup(requests.get(href).content, "lxml")
+                        body_post = detail.find('div', {'class': 'content content--full'})
+                        if body_post:
+                            # remove some web page stuff
+                            try:
+                                body_post.find('div', {'class': 'l-island-a l-mv-20 content-counters'}).decompose()
+                                body_post.find('div', {'class': 'authorCard l-mt-30'}).decompose()
+                            except Exception as ex:
+                                logger.error('Error "{}" while trying to remove unused elements from page: {}'.format(ex, href))
+                            full_text = body_post.get_text().strip()
+                            parsed_article = {
+                                'url': href,
+                                'header': h2.get_text().strip(),
+                                'text': full_text
+                            }
+                            sleep(0.5)
+                        else:
+                            logging.error("URL {} has no body.".format(href))
+                    except Exception as ex:
+                        logger.error('Error "{}" while trying to open url: {}'.format(ex, href))
+                else:
+                    # we have already stored this article in database and just need to connect it with user
+                    parsed_article = {
+                        'db_article': article
+                    }
+
+        return parsed_article
 
 
 class VcParserBuilder:
@@ -186,13 +275,41 @@ class VcParserBuilder:
         self._instance = None
 
     def __call__(self, login, password, **kwargs):
-        if not self._instance:
-            access_token = self.authorize(login, password)
-            self._instance = VcParser(access_token)
-        return self._instance
+        driver = self.authorize(login, password)
+        return VcParser(driver)
 
     def authorize(self, login, password):
-        return 'token'
+        """
+        Method authorize user within VC and return selenium driver object with all required cookies.
+
+        Receives user login and password. Generates random fake user agent.
+        Using selenium authorizes user, retrieves cookies.
+        Returns selenium web driver object for next actions.
+
+        :param login:
+        :param password:
+        :return: webdriver:driver
+        """
+
+        profile.set_preference('general.useragent.override', useragent.random)
+
+        driver = webdriver.Firefox(profile)
+
+        url = "https://vc.ru/"
+        driver.get(url)
+        driver.find_element_by_class_name('site-header-user-login__label').click()
+        auth_buttons = driver.find_elements_by_class_name('social-auth__button')
+        for button in auth_buttons:
+            if button.get_attribute('air-click') == 'auth_goto_tab':
+                button.click()
+                break
+        driver.find_element_by_name('login').send_keys(login)
+        sleep(.5)
+        driver.find_element_by_name('password').send_keys(password)
+        sleep(1)
+        driver.find_element_by_name('password').send_keys(Keys.ENTER)
+
+        return driver
 
 
 factory = ObjectFactory()
